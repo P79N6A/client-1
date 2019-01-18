d361466007c382cc98952ddb8dee1ee84fff5eb6
Antonin Sorrento <antoninsorrento@AntonindeMBP.lan>
2019-01-18 16:47:57 +0800

    💄更新UI和样式文件

--- src/layout/admin/HeaderLayout.tsx
+++ src/layout/admin/HeaderLayout.tsx
@@ -1,15 +1,39 @@
-import React, { memo, useState, Fragment } from "react";
-import { Layout, Menu, Icon, Avatar, Dropdown } from "antd";
-import { css, StyleSheet } from "aphrodite/no-important";
+import React, { memo } from "react";
+import { Menu, Icon, Avatar, Dropdown, Row, Col } from "antd";
+import styled from "styled-components";
 
 interface IProps {
   collapsed: boolean;
 
+  mbToggle(): void;
+
   toggle(): void;
 }
 
+const HeaderStyle = styled.div`
+  & > ul {
+    line-height: 64px;
+    float: right;
+  }
+  & > span {
+    font-size: 18px;
+    line-height: 66px;
+    cursor: pointer;
+    transition: color 0.3s;
+    float: left;
+    &:hover {
+      color: #1890ff;
+    }
+  }
+  & > div {
+    height: 60px;
+    float: right;
+    align-items: center;
+  }
+`;
+
 export default memo((props: IProps) => {
-  const { toggle, collapsed } = props;
+  const { toggle, collapsed, mbToggle } = props;
   // 头像隐藏菜单
   const menu = (
     <Menu>
@@ -42,32 +66,31 @@ export default memo((props: IProps) => {
       </Menu.Item>
     </Menu>
   );
-  // 样式
-  const styles = StyleSheet.create({
-    menu: {
-      lineHeight: "62px",
-      textAlign: "right"
-    },
-    trigger: {
-      fontSize: 18,
-      lineHeight: "66px",
-      cursor: "pointer",
-      transition: "color .3s",
-      float: "left",
-      ":hover": {
-        color: "#1890ff"
-      }
-    }
-  });
 
   return (
-    <Fragment>
-      <Icon
-        className={css(styles.trigger)}
-        type={collapsed ? "menu-unfold" : "menu-fold"}
-        onClick={toggle}
-      />
-      <Menu mode="horizontal" className={css(styles.menu)}>
+    <HeaderStyle>
+      <span>
+        <Row>
+          <Col xs={0} sm={0} md={0} lg={24} xl={24} xxl={24}>
+            <Icon
+              type={collapsed ? "menu-unfold" : "menu-fold"}
+              onClick={toggle}
+            />
+          </Col>
+          <Col xs={24} sm={24} md={24} lg={0} xl={0} xxl={0}>
+            <Icon
+              type={collapsed ? "menu-unfold" : "menu-fold"}
+              onClick={mbToggle}
+            />
+          </Col>
+        </Row>
+      </span>
+      <div>
+        <Dropdown overlay={menu}>
+          <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+        </Dropdown>
+      </div>
+      <Menu mode="horizontal">
         <Menu.Item key="help">
           <Icon type="question-circle" />
           帮助
@@ -76,12 +99,7 @@ export default memo((props: IProps) => {
           <Icon type="bell" />
           信息
         </Menu.Item>
-        <Menu.Item key="user">
-          <Dropdown overlay={menu}>
-            <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
-          </Dropdown>
-        </Menu.Item>
       </Menu>
-    </Fragment>
+    </HeaderStyle>
   );
 });
--- src/routes/AppRoute.tsx
+++ src/routes/AppRoute.tsx
@@ -1,25 +1,21 @@
 import React, { lazy, memo, Suspense } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import { css, StyleSheet } from "aphrodite/no-important";
+import styled from "styled-components";
 import { Spin } from "antd";
 
 // loading 样式
+const Load = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100vw;
+  height: 100vh;
+`;
 const Loading = () => {
-  const styles = StyleSheet.create({
-    // loading动画居中
-    center: {
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "center",
-      width: "100vw",
-      height: "100vh"
-    }
-  });
-
   return (
-    <div className={css(styles.center)}>
-      <Spin delay={300} size="large" tip="拼命加载中..."/>
-    </div>
+    <Load>
+      <Spin delay={300} size="large" tip="拼命加载中..." />
+    </Load>
   );
 };
 
@@ -29,8 +25,8 @@ export default memo(() => {
   return (
     <BrowserRouter>
       <Switch>
-        <Suspense fallback={<Loading/>}>
-          <Route path="/" exact={true} component={Admin}/>
+        <Suspense fallback={<Loading />}>
+          <Route path="/" exact={true} component={Admin} />
         </Suspense>
       </Switch>
     </BrowserRouter>
